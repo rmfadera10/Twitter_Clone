ebf58ffd3d7a13512f6d9f5df5fea1e1adf63803
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2020-01-01 13:35:22 +0545

    Initial commit

---
+++ app/src/main/java/com/letstravel/twitterclone/MainActivity.java
@@ -0,0 +1,24 @@
+package com.letstravel.twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/letstravel/twitterclone/MyApplication.java
@@ -0,0 +1,14 @@
+package com.letstravel.twitterclone;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
